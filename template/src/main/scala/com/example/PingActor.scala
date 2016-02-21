50c5dc0c302e5dc33543272e5ef02f0671520419
mhashim <mhashim.gm@gmail.com>
2016-02-21 08:03:59 +0300

    Added java.time.duration to check for java 8

--- template/src/main/scala/com/example/PingActor.scala
+++ template/src/main/scala/com/example/PingActor.scala
@@ -4,20 +4,21 @@ import akka.actor.{Actor, ActorLogging, Props}
 
 class PingActor extends Actor with ActorLogging {
   import PingActor._
-  
+
   var counter = 0
   val pongActor = context.actorOf(PongActor.props, "pongActor")
+  val d: Option[java.time.Duration] = None
 
   def receive = {
-  	case Initialize => 
+  	case Initialize =>
 	    log.info("In PingActor - starting ping-pong")
-  	  pongActor ! PingMessage("ping")	
+  	  pongActor ! PingMessage("ping")
   	case PongActor.PongMessage(text) =>
   	  log.info("In PingActor - received message: {}", text)
   	  counter += 1
   	  if (counter == 3) context.system.shutdown()
   	  else sender() ! PingMessage("ping")
-  }	
+  }
 }
 
 object PingActor {
